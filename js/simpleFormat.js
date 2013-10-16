5aa50d29a7bb9b0c7b51bcd99cf3eca0f2632ad3
Sandip Ransing <sandip@joshsoftware.com>
2013-10-16 15:46:17 +0530

    simpleFormat jQuery text helper
    `simpleFormat` jQuery helper similar to rails `simple_format` text helper

---
+++ js/simpleFormat.js
@@ -0,0 +1,35 @@
+/* =============================================================
+ * simpleFormat.js by Sandip Ransing
+ * https://github.com/sandipransing/simpleFormatJS
+ * ============================================================ */
+
+(function($) {
+   
+  /* Returns text transformed into HTML using simple formatting rules.
+   * Two or more consecutive newlines(\n\n) are considered as a paragraph and wrapped in <p> tags.
+   * One newline (\n) is considered as a linebreak and a <br /> tag is appended.
+   *
+   * You can pass any HTML attributes into html_options. These will be added to all created paragraphs.
+   */
+  $.simpleFormat = function(str, html_options, options) {
+    
+    html_options = (typeof html_options === "undefined") ? {} : html_options;
+    options = (typeof options === "undefined") ? {} : options;
+
+    str = str.replace(/\r\n?/, "\n"); // \r\n and \r -> \n
+    str = $.trim(str); // remove whitespaces
+    if(str.length) {
+      str = str.replace(/\n\n+/g, "</p><p>"); // 2+ newline  -> paragraph
+      str = str.replace(/\n/g, "<br />"); // 1 newline   -> br
+      
+      wrapper_tag = options.wrapper_tag ? options.wrapper_tag : 'p'
+      str = $('<' + escape(wrapper_tag) + '>', html_options).html(str);
+    }
+    return str;
+  };
+
+  $.fn.simpleFormat = function(html_options, options) {
+    return this.html($.simpleFormat(this.html(), html_options, options));
+  };
+ 
+})(jQuery);
